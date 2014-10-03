5e84de89ad7df78bea3373f314c2aadd3f399e26
John Norman Capule <johnnormancapule@gmail.com>
2014-10-04 03:05:20 +0800

    reseed

--- server/api/task/task.model.js
+++ server/api/task/task.model.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var TaskSchema = new Schema({
-  title: String,
+  name: String,
   description: String,
   point: Number,
   task_users: [{ type: Schema.Types.ObjectId, ref: "TaskUser"}],
