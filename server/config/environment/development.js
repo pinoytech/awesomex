3c0a618adfa87bb27bbfa81309054761f605e114
John Norman Capule <johnnormancapule@gmail.com>
2014-10-03 11:23:46 +0800

    initial commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/awesomex-dev'
+  },
+
+  seedDB: true
+};
